fc68b3df8b13778b0b58aa8924b155f3c4d0bb70
Chiranjibi Khanal <chiranjibik95@gmail.com>
2025-07-09 23:07:18 +0545

    blog data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()//requiring the env so that we can use those variables of env in app.js 
 const express = require('express')
 const connectToDatabase = require('./Database/index.js')
+
 const app=express()
+app.use(express.json())
 
 connectToDatabase()
 
@@ -17,6 +19,15 @@ app.get("/about",(req,res)=>{
 
     res.json({
         message:"This is About  page"
+    })
+})
+
+app.post("/blog",(req,res)=>{
+    console.log(req.body);
+    
+    res.status(200).json({
+         message : "Blog api hit sucessfully"
+         
     })
 })
 
